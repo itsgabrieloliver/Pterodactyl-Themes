5894ba4aff9ffcc8c3568977853b1bb7d31859a0
gabeoliver-bicode <37226472+gabeoliver-bicode@users.noreply.github.com>
2019-04-05 17:05:48 -0400

    made it so you can actually see whats happening

--- v0.7.13/obsidian-dark/install.sh
+++ v0.7.13/obsidian-dark/install.sh
@@ -4,31 +4,48 @@ if [ ! -f "artisan" ]; then
 	sleep 3
 	
 else
+	echo "--------------------------------------------------------"
 	echo "Welcome to the installer for Codex's Pterodactyl themes!"
 	echo "Beginning installation of your new theme!"
+	echo "--------------------------------------------------------"
+	sleep 6
 	
+	echo "--------------------------"
 	echo "Installing dependencies..."
+	echo "--------------------------"
+	sleep 4
 	apt install zip
 	apt install unzip
 	apt install git
-	sleep 3
+	sleep 6
 	
+	echo "-------------------------"
 	echo "Downloading your theme..."
+	echo "-------------------------"
+	sleep 4
 	mkdir -p tempfolder && cd tempfolder && git clone https://github.com/gabesystems/Pterodactyl-Themes.git
-	sleep 3
+	sleep 6
 	
+	echo "----------------------------------------"
 	echo "Merging theme into pterodactyl folder..."
+	echo "----------------------------------------"
+	sleep 4
 	cp -r Pterodactyl-Themes/v.0.7.13/obsidian-dark/public ..
 	cp -r Pterodactyl-Themes/v.0.7.13/obsidian-dark/resources ..
 	cd ..
-	sleep 3
+	sleep 6
 	
+	echo "-----------------"
 	echo "Clearing views..."
+	echo "-----------------"
+	sleep 4
 	php artisan view:clear
-	sleep 3
+	sleep 6
 	
+	echo "---------------------------------------------------------------"
 	echo "Congratulations! You the theme has successfully been installed."
 	echo "Thank you for downloading my theme! Enjoy!"
-	sleep 10
+	echo "---------------------------------------------------------------"
+	sleep 3
 fi
 	
